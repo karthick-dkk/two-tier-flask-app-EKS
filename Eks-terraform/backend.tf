ce621b4d8d406b524740494e02c6c8263ddf757e
Karthick D <128692563+karthick-dkk@users.noreply.github.com>
2024-09-13 13:22:41 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
-    key    = "EKS-Ajay/terraform.tfstate"
+    bucket = "karthick-s3-bucket" # Replace with your actual S3 bucket name
+    key    = "EKS-karthick/terraform.tfstate"
     region = "ap-south-1"
   }
 }
